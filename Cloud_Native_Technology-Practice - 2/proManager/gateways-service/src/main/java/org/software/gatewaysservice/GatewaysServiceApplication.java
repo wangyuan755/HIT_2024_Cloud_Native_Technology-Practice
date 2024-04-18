08ae71d2805c6cb323a2b2a847045c171ca45fdb
wangyuan755 <64016486+wangyuan755@users.noreply.github.com>
2024-04-18 19:45:06 +0800

    Add files via upload

---
+++ Cloud_Native_Technology-Practice - 2/proManager/gateways-service/src/main/java/org/software/gatewaysservice/GatewaysServiceApplication.java
@@ -0,0 +1,13 @@
+package org.software.gatewaysservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewaysServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewaysServiceApplication.class, args);
+    }
+
+}
---
+++ Cloud_Native_Technology-Practice - 2/proManager/gateways-service/src/test/java/org/software/gatewaysservice/GatewaysServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.software.gatewaysservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewaysServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
